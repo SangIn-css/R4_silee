e0b3c0c4f8df7a4f3970f12bae91f9120c6b5aa5
이상인(Sangin Lee) <silee@kanavi-mobility.com>
2026-01-21 16:54:41 +0900

    260121 v1

--- Core/Inc/eth.h
+++ Core/Inc/eth.h
@@ -9,9 +9,52 @@ extern "C" {
 
 #define USE_SPI1_DIRECT 1 
 
+// Common Register Addresses
+#define MR 0x0000
+#define GAR 0x0001
+#define SUBR 0x0005
+#define SHAR 0x0009
+#define SIPR 0x000F
+#define INTLEVEL 0x0013
+#define IR 0x0015
+#define IMR 0x0016
+#define SIR 0x0017
 #define SIMR 0x0018
+#define RTR 0x0019
+#define RCR 0x001B
+#define PTIMER 0x001C
+#define PMAGIC 0x001D
+#define PHAR 0x001E
+#define PSID 0x0024
+#define PMRU 0x0026
+#define UIPR 0x0028
+#define UPORTR 0x002C
+#define PHYCFGR 0x002E
+#define VERSIONR 0x0039
 
+// Socket Register Addresses
+#define Sn_MR 0x0000
+#define Sn_CR 0x0001
+#define Sn_IR 0x0002        
+#define Sn_SR 0x0003
+#define Sn_PORT 0x0004
+#define Sn_DHAR 0x0006
+#define Sn_DIPR 0x000C
+#define Sn_DPORT 0x0010
+#define Sn_MSSR 0x0012
+#define Sn_TOS 0x0015
+#define Sn_TTL 0x0016
+#define Sn_TX_FSR 0x0020
+#define Sn_TX_RD 0x0022
+#define Sn_TX_WR 0x0024
+#define Sn_RX_RSR 0x0026
+#define Sn_RX_RD 0x0028
+#define Sn_RX_WR 0x002A
+#define Sn_IMR 0x002C
+#define Sn_FRAG 0x002D
+#define Sn_KPALVTR 0x002F
 
+// BSB
 #define Common_Register 00000
 #define Socket_0_Register 00001
 #define Socket_0_Tx_Register 00010
--- Core/Src/eth.c
+++ Core/Src/eth.c
@@ -4,9 +4,9 @@
 #include "eth.h"
 #include <stdio.h>
 
-void eth_Write_Data()
+void eth_Init()
 {
-	uint16_t addr1 = SIMR;
+	uint16_t addr1 = GAR;
 	uint8_t BSB1 = Common_Register;
 
 	eth_Write_1Byte(addr1, BSB1, data);	// write data on 
@@ -21,14 +21,23 @@ void eth_Write_1Byte(uint16_t addr, uint8_t BSB, uint8_t data) {
 	ctrl_phs <<= 3;
 	ctrl_phs |= 0x04; // R/W = 1, OM = 00 
 	uint32_t val = (addr << 16) | (ctrl_phs << 8) | (data);
-	HAL_SPI_Transmit(&hspi1, &val, 2, 100);
+	HAL_SPI_Transmit(&hspi1, &val, 4, 100);
 
 	HAL_GPIO_WritePin(GPIOB, ENET_SCSn_Pin, GPIO_PIN_SET);
 }
 
-void eth_Write_nByte(uint16_t addr, uint8_t BSB, uint8_t data, int n) {
+void eth_Write_nByte(uint16_t addr, uint8_t BSB, uint8_t data[], int datasize) {
 
-	for (int i = 0; i < n; i++) {
-		eth_Write_1Byte(addr, BSB, data);
+	HAL_GPIO_WritePin(GPIOB, ENET_SCSn_Pin, GPIO_PIN_RESET);
+
+	uint16_t ctrl_phs = BSB;
+	ctrl_phs <<= 3;
+	ctrl_phs |= 0x04;
+	uint32_t val = (addr << 16) | (ctrl_phs << 8) | (data[0]);
+	HAL_SPI_Transmit(&hspi1, &val, 4, 100);
+	for(int i = 1; i < datasize; i++) {
+		HAL_SPI_Transmit(&hspi1, &data[i], 1, 100);
 	}
+
+	HAL_GPIO_WritePin(GPIOB, ENET_SCSn_Pin, GPIO_PIN_SET);
 }
